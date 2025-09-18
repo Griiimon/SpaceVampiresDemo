ed52635186b6c1ee240a94461b0311984e8baca4
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-18 21:34:30 +0200

    Update rotational_dampeners_component.gd

--- src/ship/components/rotational_dampeners_component.gd
+++ src/ship/components/rotational_dampeners_component.gd
@@ -6,8 +6,8 @@ const torque_per_tier= 0.2
 
 
 func tick(ship: Ship, tier: int, delta: float):
-	var thrust_input: float= ship.controller.thrust
-	if is_zero_approx(thrust_input):
+	var rotate_input: float= ship.controller.rotate
+	if is_zero_approx(rotate_input):
 		ship.angular_damp= get_damping(tier)
 	else:
 		ship.angular_damp= 0

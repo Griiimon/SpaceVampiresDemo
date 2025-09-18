05fdf1bbafef092cd3c0e4c6698acc3aa66bbf49
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-18 18:21:05 +0200

    Implement rotational dampeners

--- src/ship/components/rotational_dampeners_component.gd
+++ src/ship/components/rotational_dampeners_component.gd
@@ -1,1 +1,17 @@
+class_name RotationalDampenersComponent
 extends BaseShipComponent
+
+const DAMPING_PER_TIER= 0.2
+
+
+
+func tick(ship: Ship, delta: float):
+	var thrust_input: float= ship.controller.thrust
+	if is_zero_approx(thrust_input):
+		ship.angular_damp= get_damping()
+	else:
+		ship.angular_damp= 0
+
+
+func get_damping()-> float:
+	return tier * DAMPING_PER_TIER

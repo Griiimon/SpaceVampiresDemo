21e1f28f56c4de40efe4620600c7aa614021037d
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-09-19 20:42:45 +0200

    Update ai_ship_controller.gd

--- src/ship/ai_ship_controller.gd
+++ src/ship/ai_ship_controller.gd
@@ -7,8 +7,8 @@ func _physics_process(delta: float) -> void:
 	var attack_dir:= ship.position.direction_to(Global.player_ship.position)
 	rotate= ship.global_transform.y.dot(attack_dir)
 
-	if ship.global_transform.x.angle_to(attack_dir) > PI / 2:
+	if abs(ship.global_transform.x.angle_to(attack_dir)) > PI / 2:
 		rotate= 1
 
-	if randf() > abs(rotate):
+	if pow(randf(), 8) > abs(rotate):
 		rotate= 0
